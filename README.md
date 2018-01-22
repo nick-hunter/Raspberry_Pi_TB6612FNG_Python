ff84083be25dd6bbfa170a6afac9fe77bc22d278
Nick Hunter <nhunter20@wooster.edu>
2018-01-21 23:02:38 -0500

    Added example code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Raspberry_Pi_TB6612FNG_Python
 A Python library for the Raspberry Pi and TB6612FNG motor driver
+
+Example
+```python
+from motor import *
+from time import sleep #Only used for example
+
+#Motor(IN1,IN2,PWM,STANDBY,(Reverse polarity?))
+test = Motor(13,19,26,6,False)
+
+test.drive(100) #Forward 100% dutycycle
+sleep(1)
+test.drive(-100) #Backwards 100% dutycycle
+sleep(1)
+test.brake() #Short brake
+sleep(0.1)
+
+test.standby(True) #Enable standby
+test.standby(False) #Disable standby
+```
--- motor.py
+++ motor.py
@@ -1,6 +1,7 @@
 #Raspberry Pi TB6612FNG Library
 import RPi.GPIO as GPIO
 
+#See https://raspberrypi.stackexchange.com/a/12967 for more info
 GPIO.setmode(GPIO.BCM)
 #GPIO.setmode(GPIO.BOARD)
 
@@ -9,11 +10,11 @@ class Motor:
 	in2 = ""
 	pwm = ""
 	standbyPin = ""
-	
+
 	#Defaults
 	hertz = 1000
 	reverse = False #Reverse flips the direction of the motor
-	
+
 	#Constructor
 	def __init__(self, in1, in2, pwm, standbyPin, reverse):
 		self.in1 = in1
@@ -21,7 +22,7 @@ class Motor:
 		self.pwm = pwm
 		self.standbyPin = standbyPin
 		self.reverse = reverse
-		
+
 		GPIO.setup(in1,GPIO.OUT)
 		GPIO.setup(in2,GPIO.OUT)
 		GPIO.setup(pwm,GPIO.OUT)
@@ -29,17 +30,18 @@ class Motor:
 		GPIO.output(standbyPin,GPIO.HIGH)
 		self.p = GPIO.PWM(pwm, self.hertz)
 		self.p.start(0)
-	
+
+	#Speed from -100 to 100
 	def drive(self, speed):
 		#Negative speed for reverse, positive for forward
 		#If necessary use reverse parameter in constructor
 		dutyCycle = speed
 		if(speed < 0):
 			dutyCycle = dutyCycle * -1
-			
+
 		if(self.reverse):
 			speed = speed * -1
-		
+
 		if(speed > 0):
 			GPIO.output(self.in1,GPIO.HIGH)
 			GPIO.output(self.in2,GPIO.LOW)
@@ -47,15 +49,15 @@ class Motor:
 			GPIO.output(self.in1,GPIO.LOW)
 			GPIO.output(self.in2,GPIO.HIGH)
 		self.p.ChangeDutyCycle(dutyCycle)
-	
+
 	def brake(self):
 		self.p.ChangeDutyCycle(0)
 		GPIO.output(self.in1,GPIO.HIGH)
 		GPIO.output(self.in2,GPIO.HIGH)
-		
+
 	def standby(self, value):
 		self.p.ChangeDutyCycle(0)
 		GPIO.output(self.standbyPin,value)
-			
+
 	def __del__(self):
 		GPIO.cleanup()
